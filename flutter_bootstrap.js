310039100f6d446de1f8fcc5b53e81bfc0f30365
k8 you <k8you@local>
2026-07-15 12:38:23 +0900

    日本語フォント(Noto Sans JP)をバンドル(豆腐化け修正)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "348278209" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1150669227" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
