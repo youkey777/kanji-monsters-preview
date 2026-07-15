88219b0da540612647d62e63ebf2a48c84735b69
k8 you <k8you@local>
2026-07-15 13:35:18 +0900

    機能更新: 進化システム・ガイド3モード・きょうのかんじ・ストリーク・たましい

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "983808663" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3733759362" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
