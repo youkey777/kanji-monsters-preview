06df6315576b20781ec2e11d7c5cc6e5fb2c8a5b
k8 you <k8you@local>
2026-07-15 13:21:38 +0900

    drift Web修正: DriftWebOptionsを明示(完走時の例外解消)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1150669227" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "983808663" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
